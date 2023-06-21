51d0de7207698f1eb4c2bacfe426a845e91e8e38
AAPullen <36434489+AAPullen@users.noreply.github.com>
2023-06-21 19:34:16 -0400

    Apullen (#6)
    * completed scaffolding and added database controller
    
    * tested getting users from database
    
    * added user validation method on the db controller
    
    * working login method
    
    * backend login
    
    * added azblobservice DI
    
    * Remove unused endpoints
    
    ---------
    
    Co-authored-by: cr0schma <72145440+cr0schma@users.noreply.github.com>

--- Backend/GrandClickrApp/GrandClickr/Program.cs
+++ Backend/GrandClickrApp/GrandClickr/Program.cs
@@ -1,8 +1,31 @@
 using GrandClickr.Services;
+using GrandClickr.Models;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<AzBlobService>();
+builder.Services.AddScoped<GrandClickrContext>();
+
+// Add Cors policy
+builder.Services.AddCors(
+    options =>
+    {
+        options.AddPolicy(
+            name: "AllowAny",
+            builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()
+            );
+    }
+);
+
+// Let's add our DbContext that creates the connection to our SQL Server
+builder.Services.AddDbContext<GrandClickrContext>(options =>
+{
+    options.UseSqlServer(
+        builder.Configuration["ConnectionStrings:GrandClickrContextConnection"]); // <----- Make sure you are using the correct connection string. See your appsettings.json
+});
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -18,6 +41,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("AllowAny");
 
 app.UseHttpsRedirection();
 
